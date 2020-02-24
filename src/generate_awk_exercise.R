7665a4d189b233d419acce88c3c5f130d10a1a13
Giovanni DallOlio <dalloliogm@gmail.com>
2020-02-24 15:15:01 +0000

    several improvements to awk, started sed, more changes

--- src/generate_awk_exercise.R
+++ src/generate_awk_exercise.R
@@ -4,8 +4,9 @@ library(Homo.sapiens)
 library(rtracklayer)
 library(dplyr)
 
-chr8 = genes(TxDb.Hsapiens.UCSC.hg19.knownGene) %>% subset(seqnames %in% c("chr22", "chr8", "chr21"))
-chr8$symbol = AnnotationDbi::select(org.Hs.eg.db, key=chr8$gene_id, keytype='ENTREZID', columns='SYMBOL')$SYMBOL
-chr8$exerc1 = ifelse(start(chr8)>5000000 & end(chr8) < 10000000, T, F)
+genome = genes(TxDb.Hsapiens.UCSC.hg19.knownGene) %>% subset(seqnames %in% c("chr22", "chr8", "chr21"))
+#genome = genes(TxDb.Hsapiens.UCSC.hg19.knownGene) %>% subset(seqnames %in% c("chr22", "chr21"))
+genome$symbol = AnnotationDbi::select(org.Hs.eg.db, key=genome$gene_id, keytype='ENTREZID', columns='SYMBOL')$SYMBOL
+genome$exerc1 = ifelse(start(chr8)>5000000 & end(chr8) < 10000000, T, F)
 
-export(chr8, "exercises/genes/human_genome_subset.gff", "gff3")
+export(genome, "exercises/genes/human_genome_subset.gff", "gff3")
